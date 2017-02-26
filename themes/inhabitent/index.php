3e341d17fb397e3402b85210b8686b59e916d87e
njohnstud <n.e.johnston@hotmail.com>
2017-02-25 17:29:05 -0800

    Added buttons, fxing front-page alignment

--- themes/inhabitent/index.php
+++ themes/inhabitent/index.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-
+<section class="container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -35,6 +35,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php get_sidebar(); ?>
+</section>
+
 <?php get_footer(); ?>
--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-
+	<section class="container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -20,4 +20,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
+</section>
 <?php get_footer(); ?>
